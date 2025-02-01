944dc68347e2e757f08c9941fefc34324cf651d1
Guofeng Zhang <guofengzh@gmail.com>
2025-02-01 15:32:07 +0800

    Upgrade to Tailwind CSS v4.0.2

--- src/shared/tailwind.element.ts
+++ src/shared/tailwind.element.ts
@@ -3,6 +3,7 @@ import {LitElement, unsafeCSS} from "lit";
 import style from "./tailwind.global.css?inline";
 
 const tailwindElement = unsafeCSS(style);
+console.log(style);
 
 export const TailwindElement = (style) =>
     class extends LitElement {
